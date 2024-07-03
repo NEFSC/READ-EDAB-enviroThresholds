de62a6959b6873d7080f97caf282d26c5f0bd55f
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2024-07-03 10:48:37 -0400

    review summary and second screening

--- R/screening_analysis.R
+++ R/screening_analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 data.orig = read.csv(here::here('data','screening_results.csv')) %>%
   mutate(Answers = gsub("\"","",Answers,fixed = T),
          Question.N = paste0('Q',Question.N))%>%
-  select(Title, Question.N,Answers)%>%
+  select(-Question)%>%
   tidyr::spread(Question.N, Answers)
 
 data.include = data.orig %>%
@@ -20,3 +20,17 @@ data.spp = data.include %>%
   summarise(N = n(),
             pct = N/nrow(data.include))
 
+data.low.N = data.spp %>%
+  filter(N < 20)
+
+data.high.N = data.spp %>%
+  filter(N >= 20)
+
+data.request.now = data.orig %>%
+  filter(Q2 == 'Yes, include the reference' & Q3 %in% data.low.N$Q3)
+
+data.request.later = data.orig %>%
+  filter(Q2 == 'Yes, include the reference' & Q3 %in% data.high.N$Q3)
+
+write.csv(data.request.now,here::here('data','fulltext_request_preliminary.csv'),row.names =F)
+write.csv(data.request.later,here::here('data','second_screening_request.csv'),row.names =F)
